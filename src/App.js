0c535b75368ffd5e5a3ad23ae7fdf96d77e1dc8c
Aissa Diafi <diafiaissa@gmail.com>
2022-10-22 14:30:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+
+function App() {
+  const [cartIsEmpty] = useState(false);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav>
+          <h1>The Ninja Clothing Company</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about/*" element={<About />} />
+          <Route path="/products/:id/*" element={<ProductDetails />} />
+          <Route path="/products" element={<Products />} />
+          <Route
+            path="/error"
+            element={
+              <di>
+                <h2>We're sorry</h2>
+                <p>There's no page!</p>
+              </di>
+            }
+          />
+          <Route path="/redirect" element={<Navigate to="/about" />} />
+          <Route
+            path="/checkout"
+            element={
+              cartIsEmpty ? <Navigate to="/products" /> : <p>checkout</p>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
